195c67c72333289df8232b498155a5e2b1277b45
Karlox01 <orlandinikarlo@gmail.com>
2024-01-15 21:25:49 +0000

    adding another serializer field so i can post the users creation date on my react front end

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -10,6 +10,7 @@ class ProfileSerializer(serializers.ModelSerializer):
     posts_count = serializers.ReadOnlyField()
     followers_count = serializers.ReadOnlyField()
     following_count = serializers.ReadOnlyField()
+    user_joined_at = serializers.ReadOnlyField(source='owner.date_joined')
 
     def get_is_owner(self, obj):
         request = self.context['request']
