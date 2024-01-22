6b80d0c50e6b6f8310b86b3174690a4336789bc5
Karlox01 <orlandinikarlo@gmail.com>
2024-01-22 11:54:20 +0000

    minor error typo

--- posts/views.py
+++ posts/views.py
@@ -48,3 +48,11 @@ class PostList(generics.ListCreateAPIView):
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a post and edit or delete it if you own it.
+    """
+    serializer_class = PostSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Post.objects.all()
