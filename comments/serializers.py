558708887d383579ef7cc98722b047116494b2b0
Karlox01 <orlandinikarlo@gmail.com>
2024-01-24 20:52:35 +0000

    able to upload images to comments now

--- comments/serializers.py
+++ comments/serializers.py
@@ -7,7 +7,7 @@ class ImageSerializer(serializers.ModelSerializer):
         model = Image
         fields = ['id', 'image']
 
-        
+
 class CommentSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
@@ -31,7 +31,7 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = [
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
-            'post', 'created_at', 'updated_at', 'content', 'images'
+            'post', 'created_at', 'updated_at', 'content', 'images',
         ]
 
 class CommentDetailSerializer(CommentSerializer):
