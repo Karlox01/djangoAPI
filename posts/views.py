f5020d853369f46280b0f30905b62145c9c56a05
Karlox01 <orlandinikarlo@gmail.com>
2024-02-24 20:13:56 +0000

    change to views for posts model to handle imagedeletion on standalone class

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.db.models import Count
-from rest_framework import generics, permissions, filters
+from rest_framework import generics, permissions, filters, status
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
 from api_main.permissions import IsOwnerOrReadOnly
 from .models import Post, PostImage
 from .serializers import PostSerializer, PostImageSerializer
@@ -62,6 +63,19 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
             PostImage.objects.filter(post=serializer.instance, id__in=deleted_image_ids).delete()
 
         serializer.save()
+
+class DeletePostImage(generics.DestroyAPIView):
+    queryset = PostImage.objects.all()
+    serializer_class = PostImageSerializer
+    permission_classes = [permissions.IsAuthenticated]  # Adjust permissions as needed
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            self.perform_destroy(instance)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 
 
