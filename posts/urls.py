00ca932b529e5973b1a172c514cc5f6a0f34ef70
Karlox01 <orlandinikarlo@gmail.com>
2024-02-24 20:38:26 +0000

    revert to previous url config for urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,6 @@ from posts import views
 
 urlpatterns = [
     path('posts/', views.PostList.as_view()),
-    path('posts/<int:pk>/delete-image/', views.DeletePostImage.as_view()),
     path('posts/<int:pk>/', views.PostDetail.as_view()),
+    path('posts/<int:pk>/delete-image/', views.DeletePostImage.as_view()),
 ]
