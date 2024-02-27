343f932c6f06e3b56118311b002e5904b8e7c122
Karlox01 <orlandinikarlo@gmail.com>
2024-02-27 20:10:08 +0000

    add admin ability to delete comments

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Comment, Image
 
-# Register your models here.
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'owner', 'post', 'created_at', 'updated_at', 'content')
+    search_fields = ['owner__username', 'post__title', 'content']
+    list_filter = ('created_at', 'updated_at', 'owner', 'post')
+    actions = ['delete_selected_comments']
+
+    def delete_selected_comments(modeladmin, request, queryset):
+        # Delete selected comments and related comment images
+        for comment in queryset:
+            comment.images.all().delete()
+            comment.delete()
+
+    delete_selected_comments.short_description = "Delete selected comments"
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('comment', 'image')
+    search_fields = ['comment__content']
+
+    # You can customize this further based on your needs
